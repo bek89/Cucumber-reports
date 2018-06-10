ad08847a3d1898d8107be12b55f6f567159d8253
ulug89 <ulugbekrahmonov77@gmail.com>
2018-06-10 15:16:32 -0400

    added vasya

--- CucumberReport/src/test/java/utilities/BrowserUtilties.java
+++ CucumberReport/src/test/java/utilities/BrowserUtilties.java
@@ -6,6 +6,7 @@ public static void main(String []args){
 	String[] names={"ALi", "Vali", "Gani", "Soli" };
 	for(int i=0;i<names.length;i++){
 		System.out.println(names[i]);
+	   System.out.println("Qalisan Vasya");
 	}
 }
 
