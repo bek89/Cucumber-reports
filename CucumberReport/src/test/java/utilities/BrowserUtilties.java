3c5a22e0099d264e4ab67957a2422eb393ae32f2
ulug89 <ulugbekrahmonov77@gmail.com>
2018-06-10 17:11:19 -0400

    wkle

--- CucumberReport/src/test/java/utilities/BrowserUtilties.java
+++ CucumberReport/src/test/java/utilities/BrowserUtilties.java
@@ -6,8 +6,9 @@ public static void main(String []args){
 	String[] names={"ALi", "Vali", "Gani", "Soli" };
 	for(int i=0;i<names.length;i++){
 		System.out.println(names[i]);
-	   System.out.println("I will not work on weekends");
 	   System.out.println(names + "will work on weekends because he has lot to do lekegin kdsflksldfnkdjnd");
+	    
+	
 	}
 }
 
