32b4aef18099a12c785d325a94e1aacd16ee3fb1
ulug89 <ulugbekrahmonov77@gmail.com>
2018-06-10 16:50:15 -0400

    added some random msg

--- CucumberReport/src/test/java/utilities/BrowserUtilties.java
+++ CucumberReport/src/test/java/utilities/BrowserUtilties.java
@@ -6,7 +6,8 @@ public static void main(String []args){
 	String[] names={"ALi", "Vali", "Gani", "Soli" };
 	for(int i=0;i<names.length;i++){
 		System.out.println(names[i]);
-	   System.out.println("Qalisan Vasya");
+	   System.out.println("I will not work on weekends");
+	   System.out.println(names + "do not work on weekends");
 	}
 }
 
